a7372a77e64073aab7a4050c53b229714edd5977
Carlos <fer.1972@hotmail.com>
2019-09-17 14:00:24 -0300

    Crud em NodeJS

---
+++ node-api/src/models/Product.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const ProductSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        require: true,
+    },
+    description:{
+        type: String,
+        require: true,
+    },
+    url:{
+        type: String,
+        require: true,
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now,
+    }
+});
+
+ProductSchema.plugin(mongoosePaginate);
+
+mongoose.model('Product', ProductSchema);
